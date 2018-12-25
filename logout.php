901ca8262d1b0876ea0cb119878c3cb205026685
chintyadewi <chintyadewi437@gmail.com>
2018-12-25 09:28:39 +0700

    add alert

--- logout.php
+++ logout.php
@@ -1,10 +1,6 @@
 <?php
     session_start();
     if(session_destroy()){
-        header("Location: index.php");
+        header("location:index.php");
     }
 ?>
-<script>
-	alert("Anda akan keluar"); 
-	document.location.href="index.php";
-</script>
